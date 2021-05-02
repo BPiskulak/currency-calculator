01220cb573e92465f0fbb0278e58967a0a3545e0
Bogusław Piskulak <boguslaw.piskulak@gmail.com>
2021-05-02 13:49:35 +0200

    update js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
 
+{
+  const form = document.querySelector(".js-form");
+  const valuePln = document.querySelector(".js-valuePln");
+  const valueEuro = document.querySelector(".js-euro");
+  const valueDolar = document.querySelector(".js-dolar");
+  const valueFunt = document.querySelector(".js-funt");
+  const valueFrank = document.querySelector(".js-frank");
 
-let form = document.querySelector(".js-form");
-let valuePln = document.querySelector(".js-valuePln");
-let valueEuro = document.querySelector(".js-euro");
-let valueDolar = document.querySelector(".js-dolar");
-let valueFunt = document.querySelector(".js-funt");
-let valueFrank = document.querySelector(".js-frank");
-let inner__frank = document.querySelector(".inner__frank");
+  const calculateResult = (event) => {
+    event.preventDefault();
+    const pln = valuePln.value;
+    const euro = pln / 4.54;
+    const dolar = pln / 3.78;
+    const funt = pln / 5.26;
+    const frank = pln / 4.13;
 
+    valueEuro.innerText = euro.toFixed(2) + " €";
+    valueDolar.innerText = dolar.toFixed(2) + " $";
+    valueFunt.innerText = funt.toFixed(2) + " £";
+    valueFrank.innerText = frank.toFixed(2) + "₣";
+  };
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let Pln = valuePln.value;
-  let Euro = Pln / 4.54;
-  let Dolar = Pln / 3.78;
-  let Funt = Pln / 5.26;
-  let Frank = Pln / 4.13;
-
-
-  valueEuro.innerText = Euro.toFixed(2) + " €";
-  valueDolar.innerText = Dolar.toFixed(2) + " $";
-  valueFunt.innerText = Funt.toFixed(2) + " £";
-  valueFrank.innerText = Frank.toFixed(2) + "₣";
-
-  form.addEventListener("reset", () => {
+  const resetResult = () => {
     valueEuro.innerText = "0 €";
     valueDolar.innerText = "0 $";
     valueFunt.innerText = "0 £";
     valueFrank.innerText = "0 F";
-  });
-});
+  };
+
+  const init = () => {
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", calculateResult);
+    form.addEventListener("reset", resetResult)
+  };
+
+  init();
+
+}
